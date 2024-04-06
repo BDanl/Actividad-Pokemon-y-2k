cd316bb11fecb8066d427ad604425227f5b25c6c
BDanl <alejandrobovea@gmail.com>
2024-04-05 20:41:59 -0500

    end pokemon with interface

--- pokemon/Pokemon.cs
+++ pokemon/Pokemon.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace pokemon
 {
-    public class Pokemon
+    interface IAcciones
+    {
+         public int Attack(int[] atk);
+         public int Defense(int[] def);
+         public int GstatAtk();
+         public int GstatDef();
+
+    }
+
+    public class Metodos: IAcciones
+    {
+        public int Attack(int[] atk)
+        {
+            Random random = new();
+
+            double[] numAtk = [1, 0, 0.5];
+            int rAtk = random.Next(0, numAtk.Length);
+
+            int result = random.Next(0, atk.Length);
+            result = (int)(atk[result] * numAtk[rAtk]);
+            return result;
+
+        }
+        public int Defense(int[] def)
+        {
+            Random random = new();
+
+            double[] numDef = [1, 0.5];
+            int rDef = random.Next(0, numDef.Length);
+
+            int result2 = random.Next(0, def.Length);
+            result2 = (int)(def[result2] * numDef[rDef]);
+            return result2;
+        }
+        public int GstatAtk()
+        {
+            Random random = new();
+            int atkNumber = random.Next(0, 40);
+            return atkNumber;
+        }
+        public int GstatDef()
+        {
+            Random random = new();
+            int defNumber = random.Next(10, 35);
+            return defNumber;
+        }
+    }
+    public class Pokemon 
     {
         private string nombre;
         private string tipo;
@@ -27,5 +74,7 @@ namespace pokemon
         public int StatSalud { get => statSalud; set => statSalud = value; }
         public int[] StatAtk { get => statAtk; set => statAtk = value; }
         public int[] StatDef { get => statDef; set => statDef = value; }
+
     }
+
 }
--- pokemon/Torneo.cs
+++ pokemon/Torneo.cs
@@ -1,9 +1,13 @@
-﻿namespace pokemon
+﻿
+namespace pokemon
 {
     public class Torneo
     {
+        
         public static void Main(string[] args)
         {
+            Metodos actions = new();
+            //pokemon 1
             Console.WriteLine("Pokemon 1 name: ");
             string name = Console.ReadLine();
 
@@ -18,13 +22,13 @@
             for (int i = 0; i < 3; i++)
             {
 
-                pokemon1.StatAtk[i] = GstatAtk();
+                pokemon1.StatAtk[i] = actions.GstatAtk();
             }
 
             for (int i = 0; i < 2; i++)
             {
 
-                pokemon1.StatDef[i] = GstatDef();
+                pokemon1.StatDef[i] = actions.GstatDef();
             }
 
             //pokemon 2
@@ -42,20 +46,20 @@
             for (int i = 0; i < 3; i++)
             {
 
-                pokemon2.StatAtk[i] = GstatAtk();
+                pokemon2.StatAtk[i] = actions.GstatAtk();
             }
 
             for (int i = 0; i < 2; i++)
             {
 
-                pokemon2.StatDef[i] = GstatDef();
+                pokemon2.StatDef[i] = actions.GstatDef();
             }
 
             //Combate
             for (int i = 0; i < 3; i++)
             {
-                int atk1p1 = Attack(pokemon1.StatAtk);
-                int def1p2 = Defense(pokemon2.StatDef);
+                int atk1p1 = actions.Attack(pokemon1.StatAtk);
+                int def1p2 = actions.Defense(pokemon2.StatDef);
                 if (atk1p1 > def1p2)
                 {
                     pokemon2.StatSalud = pokemon2.StatSalud - (atk1p1 - def1p2);
@@ -65,8 +69,8 @@
                     pokemon1.StatSalud = pokemon1.StatSalud - (def1p2 - atk1p1);
                 }
 
-                int atk1p2 = Attack(pokemon2.StatAtk);
-                int def1p1 = Defense(pokemon1.StatDef);
+                int atk1p2 = actions.Attack(pokemon2.StatAtk);
+                int def1p1 = actions.Defense(pokemon1.StatDef);
                 if (atk1p2 > def1p1)
                 {
                     pokemon1.StatSalud = pokemon1.StatSalud - (atk1p2 - def1p1);
@@ -89,45 +93,6 @@
             {
                 Console.WriteLine(pokemon2.Nombre + " ganó");
             }
-        }
-
-        public static int Attack(int[] atk)
-        {
-            Random random = new();
-
-            double[] numAtk = [1, 0, 0.5];
-            int rAtk = random.Next(0, numAtk.Length);
-
-            int result = random.Next(0, atk.Length);
-            result = (int)(atk[result] * numAtk[rAtk]);
-            return result;
-
-        }
-
-        public static int Defense(int[] def)
-        {
-            Random random = new();
-
-            double[] numDef = [1, 0.5];
-            int rDef = random.Next(0, numDef.Length);
-
-            int result2 = random.Next(0, def.Length);
-            result2 = (int)(def[result2] * numDef[rDef]);
-            return result2;
-        }
-
-        public static int GstatAtk()
-        {
-            Random random = new();
-            int atkNumber = random.Next(0, 40);
-            return atkNumber;
-        }
-
-        public static int GstatDef()
-        {
-            Random random = new();
-            int defNumber = random.Next(10, 35);
-            return defNumber;
         }
     }
 }
